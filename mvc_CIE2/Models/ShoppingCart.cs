38cbc8db631e744872093845e9910049b6b200e5
Ayushi Thumar <165537687+AThumar@users.noreply.github.com>
2025-03-11 18:18:30 +0530

    shoppingCart

---
+++ mvc_CIE2/Models/ShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mvc_CIE2.Models
+{
+    public class ShoppingCart
+    {
+        [Table("ShoppingCart")]
+
+        public int Id { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        public bool IsDeleted { get; set; } = false;
+
+        public ICollection<CartDetail> CartDetails { get; set; }
+    }
+}
