93c988ce54cf1d3f8b4fec22173faccdd907a0c9
Ayushi Thumar <165537687+AThumar@users.noreply.github.com>
2025-03-11 18:15:23 +0530

    Add project files.

---
+++ mvc_CIE2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mvc_CIE2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
